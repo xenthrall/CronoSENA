c526bc849845ecf0fd47fb861826220900ec6e29
xenthrall <xenthrall0@gmail.com>
2025-10-02 11:29:29 -0500

    new: modelo tabla pivot programa_competencia y migracion

---
+++ src/database/migrations/2025_10_02_124848_create_programa_competencia_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('programa_competencia', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('programa_id')->constrained('programas')->onDelete('cascade');
+            $table->foreignId('competencia_id')->constrained('competencias')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('programa_competencia');
+    }
+};
