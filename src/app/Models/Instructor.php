31d81e99713fa44c6027d4a29b1f1185741f197f
xenthrall <xenthrall0@gmail.com>
2025-10-05 15:25:32 -0500

    feat:modelo, relaciones y migracion Instructores

---
+++ src/app/Models/Instructor.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Instructor extends Model
+{
+     protected $table = 'instructores';
+    protected $fillable = [
+        'documento',
+        'tipo_documento',
+        'nombre_completo',
+        'correo',
+        'telefono',
+        'equipo_ejecutor_id',
+        'profesion_id',
+        'especialidad',
+        'foto_url',
+        'activo',
+    ];
+
+    /**
+     * Relaciones
+     */
+
+    // Un instructor pertenece a un equipo ejecutor
+    public function equipoEjecutor()
+    {
+        return $this->belongsTo(EquipoEjecutor::class, 'equipo_ejecutor_id');
+    }
+
+    /*/ Un instructor pertenece a una profesión
+    public function profesion()
+    {
+        return $this->belongsTo(Profesion::class, 'profesion_id');
+    }
+    */
+}
---
+++ src/database/migrations/2025_10_05_014339_create_instructores_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('instructores', function (Blueprint $table) {
+            $table->id();
+            $table->string('documento')->unique();
+            $table->string('tipo_documento')->nullable();
+            $table->string('nombre_completo');
+            $table->string('correo')->unique();
+            $table->string('telefono')->nullable();
+
+            // Relaciones
+            $table->foreignId('equipo_ejecutor_id')
+                ->constrained('equipo_ejecutores')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+
+            /*$table->foreignId('profesion_id')
+                ->constrained('profesiones')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            */
+
+            $table->string('especialidad')->nullable();
+            $table->string('foto_url')->nullable();
+            $table->boolean('activo')->default(true);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instructores');
+    }
+};
