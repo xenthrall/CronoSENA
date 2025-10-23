10a99671b3b5d64170ead44220f2206616025451
xenthrall <xenthrall0@gmail.com>
2025-10-23 18:31:10 -0500

    model: agrega canAccesPanel a user controlando acceso al panel admin y planificacion

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -47,4 +50,16 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($panel->getId() === 'admin') {
+            return $this->hasRole('admin');
+        }
+        if ($panel->getId() === 'planificacion') {
+            return $this->hasRole('planificador');
+        }
+        return false;
+    }
+
 }
