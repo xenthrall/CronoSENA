64bfce66bc7ad4f40b0bb6cb2d267ed80b123932
xenthrall <xenthrall0@gmail.com>
2025-10-23 19:26:15 -0500

    feat: agrega resource users con gestion basica, listar y crear nuevo usuario

---
+++ src/app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\IconColumn;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nombre')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Correo electrónico')
+                    ->searchable(),
+                TextColumn::make('roles.name') // relación Spatie
+                    ->label('Rol')
+                    ->badge()
+                    ->colors([
+                        'primary' => 'admin',
+                        'success' => 'planificador',
+                    ])
+                    ->sortable(),
+
+                IconColumn::make('email_verified_at')
+                    ->label('Verificado')
+                    ->default(false)
+                    ->boolean(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
