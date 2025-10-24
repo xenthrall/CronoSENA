def438e012b55a274027938a3b9697af011cc56a
xenthrall <xenthrall0@gmail.com>
2025-10-24 18:49:40 -0500

    feat: agrega metodos para sincronizar y rellenar permisos en la gestion de permisos de un rol

--- src/app/Filament/Resources/Roles/Pages/EditRole.php
+++ src/app/Filament/Resources/Roles/Pages/EditRole.php
@@ -5,10 +5,13 @@ namespace App\Filament\Resources\Roles\Pages;
 use App\Filament\Resources\Roles\RoleResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Spatie\Permission\Models\Permission;
 
 class EditRole extends EditRecord
 {
     protected static string $resource = RoleResource::class;
+
+    protected array $permissionsToSync = [];
 
     protected function getHeaderActions(): array
     {
@@ -16,4 +19,48 @@ class EditRole extends EditRecord
             DeleteAction::make(),
         ];
     }
+
+
+   protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Extraer IDs de permisos desde el array anidado
+        $nested = $data['permissions'] ?? [];
+
+        $this->permissionsToSync = collect($nested)
+            ->flatMap(fn ($arr) => is_array($arr) ? $arr : [])
+            ->filter()
+            ->values()
+            ->all();
+
+        unset($data['permissions']);
+
+        return $data;
+    }
+
+    protected function afterSave(): void
+    {
+        if (! empty($this->permissionsToSync)) {
+            $permissions = Permission::whereIn('id', $this->permissionsToSync)->get();
+            $this->record->syncPermissions($permissions);
+        } else {
+            $this->record->syncPermissions([]); // Limpia todos si no hay seleccionados
+        }
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        // Esto rellena los permisos actuales 
+        $ids = $this->record->permissions()->pluck('id')->toArray();
+
+        $grouped = Permission::whereIn('id', $ids)
+            ->get()
+            ->groupBy('model')
+            ->map(fn ($items) => $items->pluck('id')->toArray())
+            ->toArray();
+
+        $data['permissions'] = $grouped;
+
+        return $data;
+    }
+
 }
