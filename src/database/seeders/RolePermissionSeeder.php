980ec2ccfc860cfa20e005b7bff34d3afac81b57
xenthrall <xenthrall0@gmail.com>
2025-10-24 19:32:19 -0500

    refactor(seeder): RolePermissionSeeder solo crea roles

--- src/database/seeders/RolePermissionSeeder.php
+++ src/database/seeders/RolePermissionSeeder.php
@@ -13,19 +13,5 @@ class RolePermissionSeeder extends Seeder
         $admin = Role::firstOrCreate(['name' => 'admin']);
         $planificador = Role::firstOrCreate(['name' => 'planificador']);
 
-        // Permisos
-        $permisos = [
-            'crear_horarios',
-            'editar_horarios',
-            'ver_instructores',
-        ];
-
-        foreach ($permisos as $permiso) {
-            Permission::firstOrCreate(['name' => $permiso]);
-        }
-
-        // Asignar permisos
-        $planificador->givePermissionTo(['crear_horarios', 'editar_horarios']);
-        $admin->givePermissionTo(Permission::all());
     }
 }
