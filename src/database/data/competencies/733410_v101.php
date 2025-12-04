3345d1dfe29a75f296d453f99c8bc75a42b91368
xenthrall <xenthrall0@gmail.com>
2025-12-04 08:42:31 -0500

    agrega programa 733410_v101

---
+++ src/database/data/competencies/733410_v101.php
@@ -0,0 +1,61 @@
+<?php
+// Competencias PRODUCCION AGROPECUARIA - PROGRAMA 733410 V101
+//Tecnico
+return [
+    [
+        'competency_type_id' => 2,
+        'code' => '240201501',
+        'name' => 'COMPRENDER TEXTOS EN INGLÉS EN FORMA ESCRITA Y AUDITIVA',
+        'description' => 'COMPRENDER TEXTOS EN INGLÉS EN FORMA ESCRITA Y AUDITIVA',
+        'duration_hours' => 180,
+    ],
+    [
+        'competency_type_id' => 1,
+        'code' => '270501031',
+        'name' => 'EJECUCION DE LABORES PECUARIAS',
+        'description' => 'COORDINAR PRODUCCIÓN DE ESPECIE PECUARIA SEGÚN OBJETIVOS DE LA EMPRESA.',
+        'duration_hours' => 550,
+    ],
+    [
+        'competency_type_id' => 1,
+        'code' => '270401058',
+        'name' => 'RECOLECCION DEL PRODUCTO',
+        'description' => 'COSECHAR EL PRODUCTO SIGUIENDO LAS ESPECIFICACIONES TÉCNICAS DEFINIDAS.',
+        'duration_hours' => 50,
+    ],
+    [
+        'competency_type_id' => 2,
+        'code' => '210201501',
+        'name' => 'EJERCICIO DE LOS DERECHOS FUNDAMENTALES DEL TRABAJO',
+        'description' => 'Ejercer derechos fundamentales del trabajo en el marco de la constitución política y los convenios internacionales.',
+        'duration_hours' => 48,
+    ],
+    [
+        'competency_type_id' => 1,
+        'code' => '270401053',
+        'name' => 'MANTENIMIENTO DEL CULTIVO',
+        'description' => 'MANTENER EL CULTIVO SEGÚN RECOMENDACIONES AGRONÓMICAS.',
+        'duration_hours' => 460,
+    ],
+    [
+        'competency_type_id' => 1,
+        'code' => '270401004',
+        'name' => 'PREPARACION DEL TERRENO',
+        'description' => 'PREPARAR TERRENO PARA SIEMBRA SEGÚN CULTIVO Y REQUERIMIENTOS TÉCNICOS.',
+        'duration_hours' => 80,
+    ],
+    [
+        'competency_type_id' => 2,
+        'code' => '240201500',
+        'name' => 'PROMOVER LA INTERACCIÓN IDÓNEA CONSIGO MISMO, CON LOS DEMÁS Y CON LA NATURALEZA EN LOS CONTEXTOS LABORAL Y SOCIAL',
+        'description' => 'PROMOVER LA INTERACCIÓN IDÓNEA CONSIGO MISMO, CON LOS DEMÁS Y CON LA NATURALEZA EN LOS CONTEXTOS LABORAL Y SOCIAL',
+        'duration_hours' => 5,
+    ],
+    [
+        'competency_type_id' => 1,
+        'code' => '999999999',
+        'name' => 'RESULTADOS DE APRENDIZAJE ETAPA PRACTICA',
+        'description' => 'APLICAR EN LA RESOLUCIÓN DE PROBLEMAS REALES DEL SECTOR PRODUCTIVO, LOS CONOCIMIENTOS, HABILIDADES Y DESTREZAS PERTINENTES A LAS COMPETENCIAS DEL PROGRAMA DE FORMACIÓN ASUMIENDO ESTRATEGIAS Y METODOLOGÍAS DE AUTOGESTIÓN',
+        'duration_hours' => 880,
+    ],
+];
