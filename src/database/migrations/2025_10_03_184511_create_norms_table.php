dce058e526f56e427a9f3380851f3a7d0265cacc
xenthrall <xenthrall0@gmail.com>
2025-12-03 19:44:18 -0500

    feat: agrega migracion norms y reorganiza permission_tables

---
+++ src/database/migrations/2025_10_03_184511_create_norms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('norms', function (Blueprint $table) {
+
+            //atributos code, name, description, created_at, updated_at
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('program_competency');
+    }
+};
